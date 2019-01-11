ff205c2061a096f7927f189234e74fe723027a9f
benwadmore <39808042+benwadmore@users.noreply.github.com>
2019-01-11 23:25:34 +0000

    Initial commit

---
+++ Retrosheet/Database/Staging/Tables/Personnel.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Staging].[Personnel] (
+    [PersonID]     VARCHAR (50) NULL,
+    [LastName]     VARCHAR (50) NULL,
+    [FirstName]    VARCHAR (50) NULL,
+    [PlayerDebut]  VARCHAR (50) NULL,
+    [ManagerDebut] VARCHAR (50) NULL,
+    [CoachDebut]   VARCHAR (50) NULL,
+    [UmpireDebut]  VARCHAR (50) NULL
+);
+
---
+++ Retrosheet/Database/dbo/Tables/PlayersAndCoaches.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PlayersAndCoaches] (
+    [ID]          VARCHAR (50) NULL,
+    [Last]        VARCHAR (50) NULL,
+    [First]       VARCHAR (50) NULL,
+    [Play debut]  VARCHAR (50) NULL,
+    [Mgr debut]   VARCHAR (50) NULL,
+    [Coach debut] VARCHAR (50) NULL,
+    [Ump debut]   VARCHAR (50) NULL
+);
+
