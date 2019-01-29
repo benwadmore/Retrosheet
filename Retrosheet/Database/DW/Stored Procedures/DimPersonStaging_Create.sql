598283de867708e9b1b295a0acd97ebb79893642
benwadmore <39808042+benwadmore@users.noreply.github.com>
2019-01-29 20:54:24 +0000

    Latest changes

---
+++ Retrosheet/Database/DW/Stored Procedures/DimPersonStaging_Create.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE DW.DimPersonStaging_Create
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+	
+	BEGIN TRY
+
+		BEGIN TRANSACTION;
+
+			DROP TABLE IF EXISTS DW.DimPersonStaging;
+
+			CREATE TABLE DW.DimPersonStaging(
+				PersonId int NOT NULL,
+				PersonnelCode varchar(8) NOT NULL,
+				FirstName varchar(50) NOT NULL,
+				LastName varchar(50) NOT NULL,
+				PlayerDebutDate date NULL,
+				ManagerDebutDate date NULL,
+				CoachDebutDate date NULL,
+				UmpireDebutDate date NULL
+			) ON [PRIMARY];
+
+		COMMIT TRANSACTION;
+
+	END TRY
+
+	BEGIN CATCH
+
+		ROLLBACK TRANSACTION;
+
+	END CATCH
+
+END
