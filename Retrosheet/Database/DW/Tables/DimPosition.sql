226b16d25b0d9433e9759eee789d168575bfce62
benwadmore <39808042+benwadmore@users.noreply.github.com>
2019-01-30 20:39:21 +0000

    Added placeholder fact and dimensions

---
+++ Retrosheet/Database/DW/Tables/DimPosition.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE DW.DimPosition
+(
+	PositionId INT IDENTITY(1, 1) NOT NULL,
+	PositionNumber TINYINT NOT NULL,
+	PositionName VARCHAR(20) NOT NULL,
+	CONSTRAINT PK_DimPosition PRIMARY KEY CLUSTERED (PositionId)
+)
---
+++ Retrosheet/Database/DW/Tables/FactGameScore.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE DW.FactGameSummary
+(
+	GameSummaryId INT IDENTITY(1, 1) NOT NULL,
+	CONSTRAINT PK_FactGameSummary PRIMARY KEY CLUSTERED (GameSummaryId)
+)
